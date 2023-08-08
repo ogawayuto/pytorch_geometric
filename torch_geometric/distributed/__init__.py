b03d8e2c7b45b903d14956ab25eafa5002dfcf6b
JakubPietrakIntel <jakub.pietrak@intel.com>
2023-08-08 14:14:28 +0000

    db init

--- torch_geometric/distributed/__init__.py
+++ torch_geometric/distributed/__init__.py
@@ -3,6 +3,7 @@ from .local_graph_store import LocalGraphStore
 from .partition import Partitioner
 from .dist_loader import DistLoader
 from .dist_neighbor_loader import DistNeighborLoader
+from .dist_link_neighbor_loader import DistLinkNeighborLoader
 
 __all__ = classes = [
     'LocalFeatureStore',
