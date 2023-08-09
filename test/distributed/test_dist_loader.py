4e4b0bfa24b7b4c2f02560a4b60207c8e9c374fb
JakubPietrakIntel <jakub.pietrak@intel.com>
2023-08-09 16:27:34 +0000

    test

--- test/distributed/test_dist_loader.py
+++ test/distributed/test_dist_loader.py
@@ -1,40 +1,29 @@
-import os.path as osp
-import subprocess
-from time import sleep
 import socket
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import numpy as np
 import pytest
 import torch
 
+from torch_geometric.distributed import (
+    LocalFeatureStore,
+    LocalGraphStore,
+    Partitioner,
+)
 from torch_geometric.data import Data, HeteroData
-from torch_geometric.distributed.dist_loader import DistLoader
 from torch_geometric.distributed.dist_neighbor_loader import DistNeighborLoader
-from torch_geometric.distributed.dist_link_neighbor_loader import DistLinkNeighborLoader
+from torch_geometric.datasets import FakeDataset, FakeHeteroDataset
 
-from torch_geometric.nn import GraphConv, to_hetero
-from torch_geometric.sampler.base import SubgraphType
 from torch_geometric.distributed.dist_context import DistContext, DistRole
 from torch_geometric.distributed import LocalFeatureStore
 from torch_geometric.distributed import LocalGraphStore
 
 from torch_geometric.testing import (
-    MyFeatureStore,
-    MyGraphStore,
     get_random_edge_index,
     onlyLinux,
-    onlyNeighborSampler,
-    onlyOnline,
     withPackage,
 )
-from torch_geometric.typing import WITH_PYG_LIB, WITH_TORCH_SPARSE
-from torch_geometric.utils import (
-    is_undirected,
-    sort_edge_index,
-    to_torch_csr_tensor,
-    to_undirected,
-)
+from torch_geometric.typing import WITH_METIS
 
 
 def is_subset(subedge_index, edge_index, src_idx, dst_idx):
@@ -44,15 +33,15 @@ def is_subset(subedge_index, edge_index, src_idx, dst_idx):
     mask = torch.from_numpy(np.isin(subidx.cpu().numpy(), idx.cpu().numpy()))
     return int(mask.sum()) == mask.numel()
 
-def test_homo(
+def homo_dist_neighbor_loader(
     world_size: int,
     rank: int,
     master_addr: str,
     master_port: int,
-    device = torch.device('cpu'),
-    rpc_worker_names: Dict[DistRole, List[str]] = {}
+    data: Tuple[LocalGraphStore, LocalFeatureStore],
+    input_nodes: torch.Tensor
     ):
-    
+    device = torch.device('cpu')
     current_ctx = DistContext(
         rank=rank,
         global_rank=rank,
@@ -60,37 +49,22 @@ def test_homo(
         global_world_size=world_size,
         group_name='dist-loader-homo-test'
         )
-
-
-    torch.manual_seed(12345)
-
-    node_id = torch.randperm(100)
-    x = torch.randn(100, 32)
-    y = torch.randint(0, 2, (100, ))
-    edge_id = torch.randperm(200)
-    edge_attr = torch.randn(200, 16)
-    edge_index = get_random_edge_index(100, 100, 200)
-    feat_store = LocalFeatureStore.from_data(node_id, x, y, edge_id, edge_attr)
-    graph_store = LocalGraphStore.from_data(edge_id, edge_index, num_nodes=100)
-    graph_store.meta = {"is_hetero" : False}
-    feat_store.meta = graph_store.meta
-    input_nodes = torch.range(0,99, dtype = torch.int32)
-    data = (graph_store, feat_store)
     
     loader = DistNeighborLoader(
         data,
         num_neighbors=[5] * 2,
-        batch_size=20,
+        batch_size=10,
         input_nodes=input_nodes,
         master_addr=master_addr,
         master_port=master_port,
         current_ctx=current_ctx,
-        rpc_worker_names=rpc_worker_names,
-        device=device
+        rpc_worker_names = {},
+        device=device,
+        drop_last = True
     )
 
     assert str(loader) == 'DistNeighborLoader()'
-    assert len(loader) == 5
+    assert len(loader) >= 5
 
     # batch = loader([0])
     # assert isinstance(batch, Data)
@@ -101,7 +75,7 @@ def test_homo(
         assert batch.x.device == device
         assert batch.x.size(0) <= 100
         assert batch.n_id.size() == (batch.num_nodes, )
-        assert batch.input_id.numel() == batch.batch_size == 20
+        assert batch.input_id.numel() == batch.batch_size == 10
         assert batch.x.min() >= 0 and batch.x.max() < 100
         assert batch.edge_index.device == device
         assert batch.edge_index.min() >= 0
@@ -117,8 +91,8 @@ def test_homo(
         )
         
 @onlyLinux
-def test_dist_neighbor_loader():
-
+@pytest.mark.skipif(not WITH_METIS, reason='Not compiled with METIS support')
+def test_dist_neighbor_loader(tmp_path):
             
     mp_context = torch.multiprocessing.get_context('spawn')
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -126,14 +100,36 @@ def test_dist_neighbor_loader():
     port = s.getsockname()[1]
     s.close()
     addr='localhost'
+    
+    data = FakeDataset(
+        num_graphs=1,
+        avg_num_nodes=110,
+        avg_degree=10,
+        edge_dim=2)[0]
+    num_parts = 2
+    partitioner = Partitioner(data, num_parts, tmp_path)
+    partitioner.generate_partition()
+    graph_store1 = LocalGraphStore.from_partition(tmp_path, pid=0)
+    graph_store2 = LocalGraphStore.from_partition(tmp_path, pid=1)
+    feat_store1 = LocalFeatureStore.from_partition(tmp_path, pid=0)
+    feat_store2 = LocalFeatureStore.from_partition(tmp_path, pid=1)
+    meta = {"is_hetero" : False}
+    graph_store1.meta = meta
+    graph_store2.meta = meta
+    feat_store1.meta = meta
+    feat_store2.meta = meta
+    data1 = (graph_store1, feat_store1)
+    data2 = (graph_store2, feat_store2)
 
 
-    w0 = mp_context.Process(target=test_homo,
-                            args=(2, 0, addr, port))
-    w1 = mp_context.Process(target=test_homo,
-                            args=(2, 1, addr, port))
+    w0 = mp_context.Process(target=homo_dist_neighbor_loader,
+                            args=(2, 0, addr, port, data1, feat_store1.get_global_id(None)))
+    w1 = mp_context.Process(target=homo_dist_neighbor_loader,
+                            args=(2, 1, addr, port, data2, feat_store2.get_global_id(None)))
 
     w0.start()
     w1.start()
     w0.join()
     w1.join()
+    
+    
