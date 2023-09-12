58d553962a3d0e3c652cdae1c096317a323969e2
JakubPietrakIntel <jakub.pietrak@intel.com>
2023-09-12 09:28:01 +0000

    db

--- torch_geometric/distributed/examples/node_hetero.py
+++ torch_geometric/distributed/examples/node_hetero.py
@@ -263,7 +263,7 @@ if __name__ == '__main__':
   parser.add_argument(
     "--dataset",
     type=str,
-    default='ogbn-mag',
+    default='ogbn-mags',
     help="The name of ogbn dataset.",
   )
   parser.add_argument(
