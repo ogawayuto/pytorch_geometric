a6181a389acadc3cf936cffb35a33ac3aa32bade
JakubPietrakIntel <jakub.pietrak@intel.com>
2023-09-13 09:41:53 +0000

    db

--- torch_geometric/distributed/examples/fake_data.py
+++ torch_geometric/distributed/examples/fake_data.py
@@ -19,17 +19,17 @@ num_parts = 2
 partitioner = Partitioner(data, num_parts, data_path)
 partitioner.generate_partition()
 
-data_path = "/home/pyg/graphlearn-dev/partition_fake_und"
+# data_path = "/home/pyg/graphlearn-dev/partition_fake_und"
 
-data = FakeHeteroDataset(
-    num_graphs=1,
-    avg_num_nodes=100,
-    avg_degree=3,
-    num_node_types=2,
-    num_edge_types=4,
-    edge_dim=2,
-    transform=T.ToUndirected())[0]
+# data = FakeHeteroDataset(
+#     num_graphs=1,
+#     avg_num_nodes=100,
+#     avg_degree=3,
+#     num_node_types=2,
+#     num_edge_types=4,
+#     edge_dim=2,
+#     transform=T.ToUndirected())[0]
 
-num_parts = 2
-partitioner = Partitioner(data, num_parts, data_path)
-partitioner.generate_partition()
+# num_parts = 2
+# partitioner = Partitioner(data, num_parts, data_path)
+# partitioner.generate_partition()
