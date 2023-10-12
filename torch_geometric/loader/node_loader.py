78ce8234183c4426243543dc8845cce0c5113512
kgajdamo <kinga.gajdamowicz@intel.com>
2023-10-12 10:35:05 +0000

    x e_id temporal solution, not correct results!

--- torch_geometric/loader/node_loader.py
+++ torch_geometric/loader/node_loader.py
@@ -179,7 +179,8 @@ class NodeLoader(torch.utils.data.DataLoader, AffinityMixin):
             if out.edge is not None and "e_id" not in data:
                 edge = out.edge.to(torch.long)
                 perm = self.node_sampler.edge_permutation
-                data.e_id = perm[edge] if perm is not None else edge
+                data.e_id = edge
+                # data.e_id = perm[edge] if perm is not None else edge
 
             data.batch = out.batch
             data.num_sampled_nodes = out.num_sampled_nodes
