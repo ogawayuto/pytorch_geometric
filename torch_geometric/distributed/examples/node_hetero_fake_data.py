ab5752bb24a32dee69691c66b5c503f7bfc1b0ee
kgajdamo <kinga.gajdamowicz@intel.com>
2023-10-12 10:47:07 +0000

    x add hetero fake example

--- torch_geometric/distributed/examples/node_hetero_fake_data.py
+++ torch_geometric/distributed/examples/node_hetero_fake_data.py
@@ -1,48 +1,71 @@
+from torch_geometric.distributed.dist_context import DistContext
+from torch_geometric.distributed.partition import load_partition_info
+
 import argparse
-import os.path as osp
 import time
 
 import torch
 import torch.distributed
 import torch.nn.functional as F
-from ogb.nodeproppred import Evaluator
+
 from torch.nn.parallel import DistributedDataParallel
 
-import torch_geometric.distributed as pyg_dist
-from torch_geometric.distributed import LocalFeatureStore, LocalGraphStore
-from torch_geometric.distributed.dist_context import DistContext, DistRole
-from torch_geometric.distributed.partition import load_partition_info
-from torch_geometric.nn import GraphSAGE
-from torch_geometric.typing import Tuple
+from torch_geometric.distributed import (
+    LocalFeatureStore,
+    LocalGraphStore,
+    DistNeighborLoader,
+)
+from torch_geometric.nn import HeteroConv, SAGEConv, Linear
+
+
+class HeteroGNN(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels, num_layers):
+        super().__init__()
+
+        self.convs = torch.nn.ModuleList()
+        for _ in range(num_layers):
+            conv = HeteroConv(
+                {
+                    ("v0", "e0", "v0"): SAGEConv(-1, hidden_channels),
+                    ("v0", "e0", "v1"): SAGEConv((-1, -1), hidden_channels),
+                    ("v1", "e0", "v0"): SAGEConv(
+                        (-1, -1), hidden_channels, add_self_loops=False
+                    ),
+                },
+                aggr="sum",
+            )
+            self.convs.append(conv)
+
+        self.lin = Linear(hidden_channels, out_channels)
+
+    def forward(self, x_dict, edge_index_dict):
+        for conv in self.convs:
+            x_dict = conv(x_dict, edge_index_dict)
+            x_dict = {key: x.relu() for key, x in x_dict.items()}
+        return self.lin(x_dict["v0"])
 
 
 @torch.no_grad()
-def test(model, test_loader, dataset_name):
-    evaluator = Evaluator(name=dataset_name)
+def test(model, test_loader):
     model.eval()
-    xs = []
+    y_pred = []
     y_true = []
     for i, batch in enumerate(test_loader):
-        batch_time_start = time.time()
-        x = model(batch.x, batch.edge_index)[:batch.batch_size]
-        xs.append(x.cpu())
-        y_true.append(batch.y[:batch.batch_size].cpu())
-        print(
-            f"---- test():  i={i}, batch_time={time.time() - batch_time_start} ----"
-        )
+        batch_size = batch["v0"].batch_size
+        x = model(batch.x_dict, batch.edge_index_dict)
+        x = x.argmax(dim=-1, keepdim=True)[:batch_size]
+        y_pred.append(x.cpu())
+        y_true.append(batch["v0"].y[:batch_size].cpu())
+        print(f"---- test():  i={i}, batch={batch} ----")
         del batch
         if i == len(test_loader) - 1:
             print(" ---- dist.barrier ----")
             torch.distributed.barrier()
-    xs = [t.to(torch.device("cpu")) for t in xs]
-    y_true = [t.to(torch.device("cpu")) for t in y_true]
-    y_pred = torch.cat(xs, dim=0).argmax(dim=-1, keepdim=True)
+    y_pred = torch.cat(y_pred, dim=0)
     y_true = torch.cat(y_true, dim=0).unsqueeze(-1)
-    test_acc = evaluator.eval({
-        "y_true": y_true,
-        "y_pred": y_pred,
-    })["acc"]
-    return test_acc
+    test_acc = int((y_pred == y_true).sum()) / y_pred.size(0)
+
+    return float(test_acc)
 
 
 def run_training_proc(
@@ -64,39 +87,56 @@ def run_training_proc(
     train_loader_master_port: int,
     test_loader_master_port: int,
 ):
-    graph = LocalGraphStore.from_partition(
-        osp.join(root_dir, f"{dataset_name}-partitions"), node_rank)
+    @torch.no_grad()
+    def init_params():
+        # Initialize lazy parameters via forwarding a single batch to the model:
+        batch = next(iter(train_loader))
+        batch = batch.to(torch.device("cpu"), "edge_index")
+        model(batch.x_dict, batch.edge_index_dict)
+
+    graph = LocalGraphStore.from_partition(root_dir, node_rank)
     print(f"-------- graph={graph} ")
     edge_attrs = graph.get_all_edge_attrs()
     print(f"------- edge_attrs ={edge_attrs}")
-    feature = LocalFeatureStore.from_partition(
-        osp.join(root_dir, f"{dataset_name}-partitions"), node_rank)
+    feature = LocalFeatureStore.from_partition(root_dir, node_rank)
     (
         meta,
         num_partitions,
         partition_idx,
         node_pb,
         edge_pb,
-    ) = load_partition_info(osp.join(root_dir, f"{dataset_name}-partitions"),
-                            node_rank)
-    print(
-        f"-------- meta={meta}, partition_idx={partition_idx}, node_pb={node_pb} "
-    )
+    ) = load_partition_info(root_dir, node_rank)
+    print(f"-------- meta={meta}, partition_idx={partition_idx}")
 
-    graph.num_partitions = num_partitions
-    graph.partition_idx = partition_idx
-    graph.node_pb = node_pb
-    graph.edge_pb = edge_pb
-    graph.meta = meta
+    node_pb_cat = torch.cat(list(node_pb.values()))
+    edge_pb_cat = torch.cat(list(edge_pb.values()))
 
     feature.num_partitions = num_partitions
     feature.partition_idx = partition_idx
-    feature.node_feat_pb = node_pb
-    feature.edge_feat_pb = edge_pb
+    feature.node_feat_pb = node_pb_cat
+    feature.edge_feat_pb = edge_pb_cat
     feature.meta = meta
-    feature.labels = torch.load(node_label_file)
 
+    graph.num_partitions = num_partitions
+    graph.partition_idx = partition_idx
+    graph.node_pb = node_pb_cat
+    graph.edge_pb = edge_pb_cat
+    graph.meta = meta
+    # generate some fake labels if not saved during partition making
+    num_classes = 2
+    feature.labels = torch.randint(num_classes, graph.node_pb.size())
     partition_data = (feature, graph)
+
+    # Define model inputs
+    # generate input node split for each fake partition
+    num_v0_nodes = node_pb["v0"].size(0)
+    input_nodes = torch.arange(num_v0_nodes).split(num_v0_nodes // 2)[node_rank]
+    # 50/50 train/test split
+    input_nodes = input_nodes.split(input_nodes.size(0) // 2)
+    train_idx = ("v0", input_nodes[0])
+    print("train_idx:", train_idx, train_idx[1].size(0))
+    test_idx = ("v0", input_nodes[1])
+    print("test_idx:", test_idx, test_idx[1].size(0))
 
     # Initialize graphlearn_torch distributed worker group context.
     current_ctx = DistContext(
@@ -106,19 +146,6 @@ def run_training_proc(
         global_rank=node_rank * num_training_procs_per_node + local_proc_rank,
         group_name="distributed-sage-supervised-trainer",
     )
-    current_device = torch.device("cpu")
-    rpc_worker_names = {}
-
-    # Create distributed neighbor loader for training
-    train_idx = train_idx.split(
-        train_idx.size(0) // num_training_procs_per_node)[local_proc_rank]
-
-    # Create distributed neighbor loader for testing.
-    test_idx = test_idx.split(test_idx.size(0) //
-                              num_training_procs_per_node)[local_proc_rank]
-    num_workers = 0
-    concurrency = 1
-    async_sampling = False
 
     # Initialize training process group of PyTorch.
     torch.distributed.init_process_group(
@@ -127,14 +154,25 @@ def run_training_proc(
         world_size=current_ctx.world_size,
         init_method="tcp://{}:{}".format(master_addr, training_pg_master_port),
     )
-    # Create loaders
-    train_loader = pyg_dist.DistNeighborLoader(
+
+    # Basic params
+    current_device = torch.device("cpu")
+    rpc_worker_names = {}
+    num_workers = 0
+    concurrency = 1
+    batch_size = 64
+    num_layers = 2
+    num_neighbors = [10] * num_layers
+    async_sampling = False
+
+    # Create distributed neighbor loader for training
+    train_loader = DistNeighborLoader(
         data=partition_data,
-        num_neighbors=[15, 10, 5],
+        num_neighbors=num_neighbors,
         input_nodes=train_idx,
-        batch_size=1024,
+        batch_size=batch_size,
         shuffle=True,
-        device=torch.device("cpu"),
+        device=current_device,
         num_workers=num_workers,
         concurrency=concurrency,
         master_addr=master_addr,
@@ -142,13 +180,15 @@ def run_training_proc(
         async_sampling=async_sampling,
         current_ctx=current_ctx,
         rpc_worker_names=rpc_worker_names,
+        disjoint=False,
     )
 
-    test_loader = pyg_dist.DistNeighborLoader(
+    # # Create distributed neighbor loader for testing.
+    test_loader = DistNeighborLoader(
         data=partition_data,
         num_neighbors=[-1],
         input_nodes=test_idx,
-        batch_size=4096,
+        batch_size=batch_size,
         shuffle=False,
         device=torch.device("cpu"),
         num_workers=num_workers,
@@ -158,45 +198,39 @@ def run_training_proc(
         async_sampling=async_sampling,
         current_ctx=current_ctx,
         rpc_worker_names=rpc_worker_names,
+        disjoint=False,
     )
 
-    # Define model and optimizer.
-    # torch.cuda.set_device(current_device)
+    model = HeteroGNN(
+        hidden_channels=64, out_channels=num_classes, num_layers=num_layers
+    )
 
-    model = GraphSAGE(
-        in_channels=100,
-        hidden_channels=256,
-        num_layers=3,
-        out_channels=47,
-    ).to(current_device)
-    model = DistributedDataParallel(model)
+    init_params()
+
+    model = DistributedDataParallel(model, find_unused_parameters=True)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-    print(f"----------- 444 ------------- ")
+    print(f"-----------  START TRAINING  ------------- ")
     # Train and test.
     f = open("dist_sage_sup.txt", "a+")
     for epoch in range(0, epochs):
         model.train()
         start = time.time()
-        print(f"TRAIN LOADER CHANNEL: {train_loader.channel}")
-        print(f"TEST LOADER CHANNEL: {test_loader.channel}")
-
         for i, batch in enumerate(train_loader):
-            batch_time_start = time.time()
+            batch_time = time.time()
             optimizer.zero_grad()
-            out = model(
-                batch.x,
-                batch.edge_index)[:batch.batch_size].log_softmax(dim=-1)
-            loss = F.nll_loss(out, batch.y[:batch.batch_size])
+            out = model(batch.x_dict, batch.edge_index_dict)
+            batch_size = batch["v0"].batch_size
+            out = out[:batch_size]
+            target = batch["v0"].y[:batch_size]
+            loss = F.cross_entropy(out, target)
             loss.backward()
             optimizer.step()
+            print(f"x2_worker: batch={batch}, cnt={i}, loss={loss}, time={time.time() - batch_time}")
             if i == len(train_loader) - 1:
                 print(" ---- dist.barrier ----")
                 torch.distributed.barrier()
-            print(
-                f"-------- dist_train_2nodes: i={i} batch_time={time.time() - batch_time_start} --------- "
-            )
-        print(" ---- dist.barrier ----")
+
         end = time.time()
         f.write(
             f"-- [Trainer {current_ctx.rank}] Epoch: {epoch:03d}, Loss: {loss:.4f}, Epoch Time: {end - start}\n"
@@ -204,44 +238,38 @@ def run_training_proc(
         print(
             f"-- [Trainer {current_ctx.rank}] Epoch: {epoch:03d}, Loss: {loss:.4f}, Epoch Time: {end - start}\n"
         )
-        print("\n\n\n\n\n\n")
         print(
-            "********************************************************************************************** "
+            "\n***************************************************************\n"
         )
-        print("\n\n\n\n\n\n")
 
         # Test accuracy.
-        if epoch % 5 == 0:  # or epoch > (epochs // 2):
-            test_acc = test(model, test_loader, dataset_name)
+        if epoch % 3 == 0:  # or epoch > (epochs // 2):
+            test_acc = test(model, test_loader)
             f.write(
-                f"-- [Trainer {current_ctx.rank}] Epoch: {epoch:03d} Test Accuracy: {test_acc:.4f}\n"
+                f"-- [Trainer {current_ctx.rank}] Test Accuracy: {test_acc:.4f}\n"
             )
             print(
-                f"-- [Trainer {current_ctx.rank}] Epoch: {epoch:03d} Test Accuracy: {test_acc:.4f}\n"
+                f"-- [Trainer {current_ctx.rank}] Test Accuracy: {test_acc:.4f}\n"
             )
-
-            print("\n\n\n\n\n\n")
-            print(
-                "********************************************************************************************** "
-            )
-        print("\n\n\n\n\n\n")
-
-    print(f"----------- 555 ------------- ")
+        print(
+            "\n***************************************************************\n"
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Arguments for distributed training of supervised SAGE.")
+        description="Arguments for distributed training of supervised SAGE."
+    )
     parser.add_argument(
         "--dataset",
         type=str,
-        default="ogbn-products",
+        default="ogbn-mags",
         help="The name of ogbn dataset.",
     )
     parser.add_argument(
         "--in_channel",
         type=int,
-        default=100,
+        default=128,
         help="in channel of the dataset, default is for ogbn-products",
     )
     parser.add_argument(
@@ -253,7 +281,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dataset_root_dir",
         type=str,
-        default="../../data/products",
+        default="../../data/mags",
         help="The root directory (relative path) of partitioned ogbn dataset.",
     )
     parser.add_argument(
@@ -283,7 +311,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--epochs",
         type=int,
-        default=10,
+        default=12,
         help="The number of training epochs.",
     )
     parser.add_argument(
@@ -302,22 +330,19 @@ if __name__ == "__main__":
         "--training_pg_master_port",
         type=int,
         default=11111,
-        help=
-        "The port used for PyTorch's process group initialization across training processes.",
+        help="The port used for PyTorch's process group initialization across training processes.",
     )
     parser.add_argument(
         "--train_loader_master_port",
         type=int,
         default=11112,
-        help=
-        "The port used for RPC initialization across all sampling workers of training loader.",
+        help="The port used for RPC initialization across all sampling workers of training loader.",
     )
     parser.add_argument(
         "--test_loader_master_port",
         type=int,
         default=11113,
-        help=
-        "The port used for RPC initialization across all sampling workers of testing loader.",
+        help="The port used for RPC initialization across all sampling workers of testing loader.",
     )
     args = parser.parse_args()
 
@@ -337,39 +362,11 @@ if __name__ == "__main__":
     f.write(
         f"* training process group master port: {args.training_pg_master_port}\n"
     )
-    f.write(
-        f"* training loader master port: {args.train_loader_master_port}\n")
+    f.write(f"* training loader master port: {args.train_loader_master_port}\n")
     f.write(f"* testing loader master port: {args.test_loader_master_port}\n")
 
     f.write("--- Loading data partition ...\n")
-    root_dir = osp.join(osp.dirname(osp.realpath(__file__)),
-                        args.dataset_root_dir)
-    data_pidx = args.node_rank % args.num_dataset_partitions
-    r"""
-  dataset = pyg_dist.DistDataset()
-  dataset.load(
-    root_dir=osp.join(root_dir, f'{args.dataset}-partitions'),
-    partition_idx=data_pidx,
-    node_label_file=osp.join(root_dir, f'{args.dataset}-label', 'label.pt'),
-    partition_format="pyg"
-  )
-  """
-    node_label_file = osp.join(root_dir, f"{args.dataset}-label", "label.pt")
-
-    train_idx = torch.load(
-        osp.join(
-            root_dir,
-            f"{args.dataset}-train-partitions",
-            f"partition{data_pidx}.pt",
-        ))
-    test_idx = torch.load(
-        osp.join(
-            root_dir,
-            f"{args.dataset}-test-partitions",
-            f"partition{data_pidx}.pt",
-        ))
-    train_idx.share_memory_()
-    test_idx.share_memory_()
+    root_dir = "/home/pyg/graphlearn-dev/partition_fake"
 
     f.write("--- Launching training processes ...\n")
 
@@ -381,11 +378,11 @@ if __name__ == "__main__":
             args.num_training_procs,
             args.dataset,
             root_dir,
-            node_label_file,
+            None,
             args.in_channel,
             args.out_channel,
-            train_idx,
-            test_idx,
+            None,
+            None,
             args.epochs,
             args.batch_size,
             args.master_addr,
